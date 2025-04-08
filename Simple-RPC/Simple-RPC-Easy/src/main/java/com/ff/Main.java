54e9aa69c86e993f964b7b339cf858d69a730392
FF <19970825692@163.com>
2025-04-08 12:51:57 +0800

    commit Simple-RPC-Easy module

---
+++ Simple-RPC/Simple-RPC-Easy/src/main/java/com/ff/Main.java
@@ -0,0 +1,11 @@
+package com.ff;
+
+/**
+ * @author FF
+ * @date 2025/4/8
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
